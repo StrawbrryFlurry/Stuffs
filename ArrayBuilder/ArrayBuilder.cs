bb095aedf8a0c0d2bdb1d0948b0cb5b05918489b
Michael <michi.straub@icloud.com>
2025-07-18 18:25:12 +0200

    Cooling

--- ArrayBuilder/ArrayBuilder.cs
+++ ArrayBuilder/ArrayBuilder.cs
@@ -1,212 +1,206 @@
-﻿using System.Collections.Immutable;
-using System.Numerics;
+﻿using System.Buffers;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace ArrayBuilder;
 
+public static class ArrayBuilder {
+    public const int LargeElementInitSize = 64;
+    public const int InitSize = 256;
+}
+
 public ref partial struct ArrayBuilder<TElement> {
-  private Span<TElement> _elements;
-  
-  private int _count = 0;
+    private const int DefaultCapacity = 4;
 
-  public int Count {
+    private Span<TElement> _elements;
+    private TElement[]? _rentedArray;
+    private int _count = 0;
+
+    public static ArrayBuilder<TElement> Empty => new();
+
+    public Span<TElement> Elements {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _elements[.._count];
+    }
+
+    public Span<TElement> UnsafeBuffer {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _elements;
+    }
+
+    public ArrayBuilder() { }
+
+    public ArrayBuilder(in Span<TElement> initialBuffer) {
+        _elements = initialBuffer;
+    }
+
+    internal ArrayBuilder(int initialCapacity) {
+        if (initialCapacity < 0) {
+            throw new ArgumentOutOfRangeException(nameof(initialCapacity));
+        }
+
+        var allocatedCapacity = GetCapacityForSize(initialCapacity);
+        var array = ArrayPool<TElement>.Shared.Rent(allocatedCapacity);
+        _elements = _rentedArray = array;
+    }
+
+    private static int GetCapacityForSize(int count) {
+        if (count < 0) {
+            return 0;
+        }
+
+        // Prefer common array sizes
+        return count switch {
+            <= DefaultCapacity => DefaultCapacity,
+            <= 8 => 8,
+            <= 16 => 16,
+            <= 32 => 32,
+            <= 64 => 64,
+            <= 128 => 128,
+            <= 256 => 256,
+            <= 512 => 512,
+            <= 1024 => 1024,
+            <= 2048 => 2048,
+            _ => count
+        };
+    }
+
+    public void AddRange(in ReadOnlySpan<TElement> elements) {
+        var count = _count;
+        var newCount = count + elements.Length;
+        var buffer = _elements;
+        if ((uint)newCount < (uint)buffer.Length) {
+            elements.CopyTo(_elements[count..]);
+            _count = newCount;
+            return;
+        }
+
+        Grow(newCount);
+
+        elements.CopyTo(_elements[count..]);
+        _count = newCount;
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    get => _count;
-  }
-  
-  private Span<TElement> SlicedElements {
+    public void Add(in TElement element) {
+        var count = _count;
+        var buffer = _elements;
+        if ((uint)count < (uint)buffer.Length) {
+            buffer[count] = element;
+            _count = count + 1;
+            return;
+        }
+
+        Grow(count + 1);
+        _elements[count] = element;
+        _count = count + 1;
+    }
+
+    private void Grow(int requiredCapacity) {
+        Debug.Assert(_count < requiredCapacity);
+
+        var buffer = _elements;
+        var len = buffer.Length;
+        var newCapacity = Math.Max(len == 0 ? DefaultCapacity : len * 2, requiredCapacity + 1);
+
+        var resizedElements = ArrayPool<TElement>.Shared.Rent(newCapacity);
+        buffer[.._count].CopyTo(resizedElements);
+        if (_rentedArray is not null) {
+            ArrayPool<TElement>.Shared.Return(_rentedArray);
+        }
+
+        _elements = _rentedArray = resizedElements;
+    }
+
+    public TElement[] ToArrayAndFree() {
+        var array = new TElement[_count];
+
+        Elements.CopyTo(array);
+        Dispose();
+
+        return array;
+    }
+
+    public ImmutableArray<TElement> ToImmutableArrayAndFree() {
+        // The ImmutableArray constructor will create a new array from the span,
+        // so we can give it a cut-down version of the array we rented, avoiding
+        // allocating the correctly sized array twice.
+        var array = ImmutableArray.Create(Elements);
+        Dispose();
+        return array;
+    }
+
+    public Span<TElement> ToSpanAndFree() {
+        Span<TElement> backingArray = new TElement[_count];
+
+        Elements.CopyTo(backingArray);
+        Dispose();
+
+        return backingArray;
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    get => _elements[.._count];
-  }
-
-  public static ArrayBuilder<TElement> Empty => default;
-
-  public ArrayBuilder() {
-    _elements = default;
-  }
-
-  /// <summary>
-  /// </summary>
-  /// <param name="initialCapacity"></param>
-  /// <exception cref="ArgumentOutOfRangeException"></exception>
-  public ArrayBuilder(uint initialCapacity) {
-    Grow(initialCapacity);
-  }
-
-  /// <summary>
-  /// Returns a new <see cref="Array"/> containing all elements of the builder.
-  /// The builder cannot be used after this operation.
-  /// </summary>
-  /// <returns>A new array containing all the elements of the builder</returns>
-  [SkipLocalsInit]
-  public TElement[] ToArrayAndFree() {
-    var array = new TElement[_count];
-
-    SlicedElements.CopyTo(array);
-
-    return array;
-  }
-
-  /// <summary>
-  /// Returns a new <see cref="ImmutableArray{T}"/> containing all elements of the builder.
-  /// The builder cannot be used after this operation.
-  /// </summary>
-  /// <returns>A new array containing all the elements of the builder</returns>
-  public ImmutableArray<TElement> ToImmutableArrayAndFree() {
-    // The ImmutableArray constructor will create a new array from the span,
-    // so we can give it a cut-down version of the array we rented, avoiding
-    // allocating the correctly sized array twice.
-    var array = ImmutableArray.Create(SlicedElements);
-    return array;
-  }
-
-  /// <summary>
-  /// Returns a new <see cref="Span{T}"/> containing all elements of the builder.
-  /// The builder cannot be used after this operation.
-  /// </summary>
-  /// <returns>A new span containing all the elements of the builder</returns>
-  [SkipLocalsInit]
-  public Span<TElement> ToSpanAndFree() {
-    Span<TElement> backingArray = new TElement[_count];
-
-    SlicedElements.CopyTo(backingArray);
-
-    return backingArray;
-  }
-  
-  public void CopyTo(Span<TElement> span) {
-    SlicedElements.CopyTo(span);
-  }
-
-  /// <summary>
-  /// Returns a new <see cref="ReadOnlySpan{T}"/> containing all elements of the builder.
-  /// The builder cannot be used after this operation.
-  /// </summary>
-  /// <returns></returns>
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public ReadOnlySpan<TElement> ToReadOnlySpanAndFree() {
-    return new ReadOnlySpan<TElement>(ToArrayAndFree());
-  }
-
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public TemporarySpan<TElement> AsRefForfeitOwnership() {
-    return new TemporarySpan<TElement>(SlicedElements);
-  }
-  
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public UnsafeTempSpan<TElement> AsVeryUnsafeRefForfeitOwnership() {
-    return new UnsafeTempSpan<TElement>(SlicedElements);
-  }
-
-  /// <summary>
-  /// Returns a span that includes all elements in the builder, useful for
-  /// iterating over the elements or copying them to another span.
-  /// Consumers DO NOT own the returned span and MUST NOT use it after
-  /// this builder instance has been used again, mutate it,
-  /// pass it to another method or otherwise leak it outside of it's owned context.
-  /// The backing array of the span is still used by the builder and will be changed
-  /// / updated it for future operations.
-  /// </summary>
-  /// <returns></returns>
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public ReadOnlySpan<TElement> DangerousAsSpanWithoutOwnership() {
-    return SlicedElements;
-  }
-
-  [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  internal Span<TElement> DangerousAsWriteableSpanWithoutOwnership() {
-    return SlicedElements;
-  }
-  
-  public void AddRange(IEnumerable<TElement> elements) {
-    foreach (var element in elements) {
-      Add(element);
-    }
-  }
-
-  public void AddRange(ReadOnlySpan<TElement> elements) {
-    var newCount = (_count + elements.Length);
-    var elementsRef = _elements;
-    if (newCount <= elementsRef.Length) {
-      elements.CopyTo(elementsRef[_count..]);
-      _count = newCount;
-      return;
+    public ReadOnlySpan<TElement> ToReadOnlySpanAndFree() {
+        return new ReadOnlySpan<TElement>(ToArrayAndFree());
     }
 
-    Grow((uint)newCount);
-
-    elements.CopyTo(_elements[_count..]); // Might be a new array
-    _count = newCount;
-  }
-
-  public void Add(TElement element) {
-    var count = _count;
-    var elementsRef = _elements;
-    if (count < elementsRef.Length) {
-      elementsRef[_count] = element;
-      _count++;
-      return;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public TemporarySpan<TElement> AsRefForfeitOwnership() {
+        return new TemporarySpan<TElement>(Elements, _rentedArray);
     }
 
-    Grow((uint)_count + 1);
-    _elements[_count] = element;
-    _count++;
-  }
-
-  public void GrowTo(uint capacity) {
-    Grow(capacity);
-  }
-
-  [SkipLocalsInit]
-  private void Grow(uint size) {
-    var oldBufferRef = _currentBuffer;
-    var oldElements = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer128, TElement>(ref oldBufferRef), _count);
-    Span<TElement> newBuffer;
-    switch (size) {
-      case <= 4:
-        ref var b4 = ref Unsafe.As<ArrayBuffer128, ArrayBuffer4>(ref _currentBuffer);
-        b4 = new ArrayBuffer4();
-        newBuffer = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer4, TElement>(ref b4), 4) ;
-        break;
-      case <= 16:
-        ref var b16 = ref Unsafe.As<ArrayBuffer128, ArrayBuffer16>(ref _currentBuffer);
-        b16 = new ArrayBuffer16();
-        newBuffer = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer16, TElement>(ref b16), 16);
-        break;
-      case <= 32:
-        ref var b32 = ref Unsafe.As<ArrayBuffer128, ArrayBuffer32>(ref _currentBuffer);
-        b32 = new ArrayBuffer32();
-        newBuffer = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer32, TElement>(ref b32), 32);
-        break;
-      case <= 64:
-        ref var b64 = ref Unsafe.As<ArrayBuffer128, ArrayBuffer64>(ref _currentBuffer);
-        b64 = new ArrayBuffer64();
-        newBuffer = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer64, TElement>(ref b64), 64);
-        break;
-      case <= 128 when !typeof(TElement).IsValueType:
-        ref var b128 = ref Unsafe.As<ArrayBuffer128, ArrayBuffer128>(ref _currentBuffer);
-        b128 = new ArrayBuffer128();
-        newBuffer = MemoryMarshal.CreateSpan(ref Unsafe.As<ArrayBuffer128, TElement>(ref b128), 128);
-        break;
-      default:
-        var bufferSize = BitOperations.RoundUpToPowerOf2(size);
-        var dynamicBuffer = new TElement[bufferSize];
-        newBuffer = dynamicBuffer.AsSpan();
-        break;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlySpan<TElement> DangerousAsSpanWithoutOwnership() {
+        return Elements;
     }
-    
-    if (_count != 0) {
-      oldElements.CopyTo(newBuffer);
-    }
-    
-    _elements = newBuffer;
-  }
 
-  private static void ClearBuffers() {
-    // ReSharper disable SuspiciousTypeConversion.Global
-    if (_currentBuffer is not NullBuffer) {
-      _currentBuffer = Unsafe.As<NullBuffer, ArrayBuffer128>(ref _nullBufferInstance);
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    internal Span<TElement> DangerousAsWriteableSpanWithoutOwnership() {
+        return Elements;
     }
-  }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose() {
+        var rentedArray = _rentedArray;
+        if (rentedArray is not null) {
+            ArrayPool<TElement>.Shared.Return(rentedArray);
+        }
+
+        this = default;
+    }
+}
+
+public static class ArrayBuilderExtensions {
+    public static int IndexOf<T>(this global::ArrayBuilder.ArrayBuilder<T> builder, T value) where T : IEquatable<T> {
+        return builder.DangerousAsSpanWithoutOwnership().IndexOf(value);
+    }
+
+    public static bool Contains<T>(this global::ArrayBuilder.ArrayBuilder<T> builder, T value) where T : IEquatable<T> {
+        return builder.IndexOf(value) is not -1;
+    }
+
+    public static ref T? TryGet<T>(this global::ArrayBuilder.ArrayBuilder<T> builder, T element) where T : IEquatable<T> {
+        var index = builder.IndexOf(element);
+        if (index is not -1) {
+            return ref builder.DangerousAsWriteableSpanWithoutOwnership()[index]!;
+        }
+
+        return ref Unsafe.NullRef<T>()!;
+    }
+
+    public static unsafe string ToStringAndFree(this global::ArrayBuilder.ArrayBuilder<char> builder) {
+        var chars = builder.DangerousAsSpanWithoutOwnership();
+
+        // The string ctor should copy all chars to its own buffer and
+        // allow us to free the rented array, without breaking the string.
+        // https://source.dot.net/#System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/String.cs,151
+        string str;
+        fixed (char* p = chars) {
+            str = new string(p, 0, chars.Length);
+        }
+
+        builder.Dispose();
+        return str;
+    }
 }
---
+++ ArrayBuilder/FormattableSyntaxWritable.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ArrayBuilder;
+
+[InterpolatedStringHandler]
+public ref struct FormattableSyntaxWritable {
+    private ref ValueSyntaxWriter _writer;
+
+    public int Length => _writer.Length;
+
+    public FormattableSyntaxWritable(int literalLength, int formattedCount, [CallerMemberName] string memberName = "") {
+        _writer = new ValueSyntaxWriter();
+    }
+
+    public void AppendLiteral(string s) {
+        _writer.Write(s);
+    }
+
+    public void AppendFormatted(string s) {
+        _writer.Write(s);
+    }
+
+    public readonly void CopyToAndDispose(Span<char> buffer) {
+        _writer.CopyTo(buffer);
+        _writer.Dispose();
+    }
+}
